3cb4a353f3cd652dba384a06d71a8ef30179a834
karismaja <79548645+karismaja@users.noreply.github.com>
2021-02-23 17:29:50 -0600

    Add files via upload

---
+++ Test Website/scripts/main.js
@@ -0,0 +1,26 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/TestGraphicHoriz.png');
+    } else {
+      myImage.setAttribute('src','images/TestGraphic.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to the testing ground, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to the testing ground, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
